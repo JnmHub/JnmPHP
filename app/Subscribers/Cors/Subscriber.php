5922986782411746aa5349b9f660193ff01ba31d
李金木 <122771159@qq.com>
2025-10-14 08:29:34 +0800

    # 增加支持跨域的订阅者

---
+++ app/Subscribers/Cors/Subscriber.php
@@ -0,0 +1,38 @@
+<?php
+// 文件路径: app/Subscribers/DatabaseLogger/Subscriber.php
+
+namespace App\Subscribers\Cors;
+
+use App\Core\Events\AbstractSubscriber;
+use App\Core\Http\Request;
+use Illuminate\Database\Capsule\Manager as DB;
+use Illuminate\Database\Events\QueryExecuted;
+
+class Subscriber extends AbstractSubscriber
+{
+    private const SLOW_QUERY_THRESHOLD = 100;
+
+    public function subscribe(): void
+    {
+        $this->events()->on('router.before_dispatch', function() {
+            $request = Request::capture();
+            header('Access-Control-Allow-Origin: *');
+
+            // 允许的 HTTP 方法
+            header('Access-Control-Allow-Methods: GET, POST, PUT, PATCH, DELETE, OPTIONS');
+
+            // 允许前端在请求中携带的自定义请求头
+            header('Access-Control-Allow-Headers: DNT, User-Agent, X-Requested-With, If-Modified-Since, Cache-Control, Content-Type, Range, Authorization');
+
+            // 允许浏览器在跨域请求中携带凭证（如 Cookies）
+            header('Access-Control-Allow-Credentials: true');
+
+            // 如果是 OPTIONS 预检请求，直接返回成功，不继续执行后续操作
+            if ($request->method == 'OPTIONS') {
+                http_response_code(204); // 204 No Content
+                exit();
+            }
+        });
+
+    }
+}
