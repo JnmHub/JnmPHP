71a842a6fcee9a3b6f1225d45bad73a095cad649
李金木 <122771159@qq.com>
2025-10-23 16:11:34 +0800

    增加配置信息

--- config/providers.php
+++ config/providers.php
@@ -2,6 +2,7 @@
 
 // 注册你的核心服务提供者
 use App\Providers\AppServiceProvider;
+use App\Providers\ConfigServiceProvider;
 use App\Providers\DatabaseServiceProvider;
 use App\Providers\EventServiceProvider;
 use App\Providers\LogServiceProvider;
@@ -9,6 +10,7 @@ use App\Providers\RouteServiceProvider;
 use App\Providers\ViewServiceProvider;
 
 return [
+    ConfigServiceProvider::class,
     AppServiceProvider::class,
     LogServiceProvider::class,
     EventServiceProvider::class,
